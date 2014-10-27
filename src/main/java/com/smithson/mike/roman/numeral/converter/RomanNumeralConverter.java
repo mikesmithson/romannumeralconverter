b36fa30fd0c71cb109d3cd78c67fcfee6c841a56
Mike Smithson <mikesmithso@gmail.com>
2014-10-26 23:51:45 -0400

    Next refactoring

--- src/main/java/com/smithson/mike/roman/numeral/converter/RomanNumeralConverter.java
+++ src/main/java/com/smithson/mike/roman/numeral/converter/RomanNumeralConverter.java
@@ -1,36 +1,35 @@
 package com.smithson.mike.roman.numeral.converter;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
+
 /**
  * Created by mikesmithson on 10/26/14.
  */
 public class RomanNumeralConverter {
 
+    private static final Map<Integer, String> ROMAN_NUMERAL_PRIMITIVES = new LinkedHashMap<Integer, String>();
+
+    static {
+          ROMAN_NUMERAL_PRIMITIVES.put(10, "X");
+          ROMAN_NUMERAL_PRIMITIVES.put(9, "IX");
+          ROMAN_NUMERAL_PRIMITIVES.put(5, "V");
+          ROMAN_NUMERAL_PRIMITIVES.put(4, "IV");
+          ROMAN_NUMERAL_PRIMITIVES.put(1, "I");
+
+    }
+
     public String convert(Integer integer) {
         StringBuilder result = new StringBuilder();
-        while (integer >= 10) {
-             result.append("X");
-            integer -= 10;
+        for(Map.Entry<Integer, String> entries: ROMAN_NUMERAL_PRIMITIVES.entrySet()) {
+            while (integer >= entries.getKey())  {
+                result.append(entries.getValue());
+                integer -= entries.getKey();
+            }
         }
 
-        while (integer >= 9) {
-            result.append("IX");
-            integer -= 9;
-        }
-
-        while (integer >= 5) {
-            result.append("V");
-            integer -= 5;
-        }
-
-        while (integer >= 4) {
-            result.append("IV");
-            integer -= 4;
-        }
-
-        while (integer >= 1) {
-            result.append("I");
-            integer -= 1;
-        }
         return result.toString();
     }
 }
