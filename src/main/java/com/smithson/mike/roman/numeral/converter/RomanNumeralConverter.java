2d770be3115c1498c8b9425275ecad0710b56f20
Mike Smithson <mikesmithso@gmail.com>
2014-10-26 22:35:09 -0400

    Next passing test

--- src/main/java/com/smithson/mike/roman/numeral/converter/RomanNumeralConverter.java
+++ src/main/java/com/smithson/mike/roman/numeral/converter/RomanNumeralConverter.java
@@ -7,14 +7,14 @@ public class RomanNumeralConverter {
 
     public String convert(Integer integer) {
         StringBuilder result = new StringBuilder();
-//        if (integer == 5) {
-//         result.append("V");
-//        }
-        //else {
+        if (integer == 5) {
+         result.append("V");
+        }
+        else {
             for (int i = 0; i < integer; i++) {
                 result.append("I");
             }
-       // }
+       }
         return result.toString();
     }
 }
